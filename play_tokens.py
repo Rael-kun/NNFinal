530bcf09f282c5961ba564048e45f721def1f765
eddiebfrench <117395144+eddiebfrench@users.noreply.github.com>
2025-05-04 16:23:40 -0500

    Update play_tokens.py

--- play_tokens.py
+++ play_tokens.py
@@ -10,7 +10,7 @@ def pitchstr_to_num(note_name):
     return pitch.Pitch(note_name).midi
 
 
-BPM = 120
+BPM = 480 ##################################################### REALLY FAST FOR NOW, JUST TESTING PURPOSES
 second_per_quarter_note = 60 / BPM
 
 
@@ -41,7 +41,6 @@ def play_note(midi_out, midi_note, duration, velocity=127): #velocity means volu
     time.sleep(duration)
     midi_out.note_off(midi_note, velocity)
 
-
 def quantize_duration(dur_str: str):
     STANDARD_DURATIONS = [ #what length of fractions do we allow?
         Fraction(1, 16), Fraction(1, 8), Fraction(1, 6), Fraction(1, 4),
@@ -55,7 +54,6 @@ def quantize_duration(dur_str: str):
         return float(closest)
     except Exception:
         return 0.25
-
 
 def get_duration(dur:str):
     try:
@@ -78,7 +76,7 @@ def play_tokens(tokens, instrument):
             i += 1
 
             simul_notes = []
-            while i < len(tokens) and tokens[i] != "</simul>" and i - beginning < 10: #Cannot be more than 10 notes played simultaneously
+            while i < len(tokens) and tokens[i] != "</simul>" and i - beginning < 7: #Cannot be more than 7 notes played simultaneously
                 token = tokens[i]
                 match = re.match(r"note(.+)_(\w+)", token)
                 if match:
@@ -139,9 +137,8 @@ def play_tokens(tokens, instrument):
 
 
 
-tokens = ['<simul>', 'rest_1/2', 'rest_1/2', '</simul>', '<simul>', 'noteF4_1/2', 'noteG#4_1/2', 'noteF4_1', 'noteG#4_1', '</simul>', '<simul>', 'noteF5_1/2', 'noteG#5_1/2', '</simul>', '<simul>', 'noteF5_3/2', 'noteG#5_3/2', 'noteF4_3', 'noteG#4_3', '</simul>', 'noteF5_1/5', 'noteE-4_171/512', 'noteB6_1/3', 'noteC#6_17/240', 'noteG#2_171/1024', 'noteA3_19/128', 'noteE4_1/2', 'noteC4_1', 'rest_81/16', 'noteB-6_51/512', 'noteG#2_171/1024', 'noteG#5_51/512', 'noteG1_1', 'noteC#2_1/10', 'noteE4_7/8', 'rest_23/8', 'noteG1_1/6', 'noteF#3_51/256', 'noteF#2_1/6', 'noteE-3_23/160', 'noteB3_1/10', 'noteF#6_37/240', 'noteG4_13/96', 'noteG#2_171/1024', 'noteG#3_1/24', 'noteE-7_13/96', 'noteB-6_2/5', 'noteB0_1/2', 'noteA1_1/10', 'noteC#4_341/1024', 'noteG#1_23/160', 'noteG5_1/8', 'noteA3_137/480', 'noteE6_171/512', 'noteC3_85/1024', 'noteC6_307/512', 'noteC#4_57/256', 'noteG#2_171/1024', 'noteG#6_7/4', 'noteE5_65/1024', 'noteG#2_171/1024', 'noteC5_307/512', 'noteD6_1/3', 'noteG#2_171/1024', 'noteB4_37/480', 'noteG5_1/8', 'noteA5_1/4', 'noteG#2_171/1024', 'noteC3_1/3', 'noteD6_1/3', 'noteB-2_1/3', 'rest_81/16', 'noteG1_2', 'noteA1_1/12', 'noteE5_1/2', 'noteG#4_79/1024', 'noteG#2_171/1024', 'noteC3_1/12', 'noteG#2_41/512', 'noteE-3_37/240', 'noteA4_3/8', 'noteD5_0', 'noteC#3_1/10', 'rest_145/96', 'noteE-5_2', 'noteC3_17/160', 'noteC#4_57/256', 'noteG#2_171/1024', 'noteG#6_341/512', 'noteC4_29/160', 'noteB4_617/240', 'noteA6_1/10', 'noteC#3_1/10', 'noteG7_51/1024', 'noteC6_1/6', 'noteB-3_171/1024', 'rest_1/96', 'noteC#1_4', 'noteF#3_51/256', 'noteA6_29/240', 'noteG#1_3/8', 'noteF#3_2/5']
 try:
-    play_tokens(tokens, 0)
+    play_tokens(notes, 0)
 except:
     pygame.midi.quit()
 
